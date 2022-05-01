59bbc0efbd7c315808bac1e9c8b69219ecf42700
Junki <wnsrl1228@naver.com>
2022-05-01 13:22:35 +0900

    return 추가

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -2,17 +2,17 @@
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         // 로그인 되어 있으면 쿠키값이 있고 true가 나옴
-        next();
+        return next();
     } else {
-        res.status(403).send('로그인 필요');
+        return res.status(403).send('로그인 필요');
     }
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        next();
+        return next();
     } else {
         const message = encodeURIComponent('로그인한 상태입니다.');
-        res.redirect(`/?error=${message}`);
+        return res.redirect(`/?error=${message}`);
     }
 };
