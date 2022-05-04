e04c3ace751a3b2a0b110d87244a1cd28ca36612
Junki <wnsrl1228@naver.com>
2022-05-04 14:35:39 +0900

    deserializeUser 부분 변경

--- src/passport/index.js
+++ src/passport/index.js
@@ -5,12 +5,12 @@ const pool = require('../config/db');
 module.exports = () => {
     //세션관리 
     passport.serializeUser((user, done) => {
-    done(null, user.id); 
+        done(null, user.id); 
     });
 
     passport.deserializeUser((id, done) => {
         pool.execute('SELECT * FROM user WHERE id = ?', [id])
-            .then(user => done(null, user))
+            .then(user => done(null, user[0][0]))
             .catch(err => done(err));
     });
 
